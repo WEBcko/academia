353e2bedee4d09ea42f19f619d7ac8d5fa761585
TEXTBr <FireWitch2003@outlook.com>
2023-06-02 02:49:37 -0300

    erick

---
+++ app/Http/Controllers/Site/SiteController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+class SiteController{
+
+    public function action(){
+        return view('home');
+    }
+}
+
+
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="js/index.js"></script>
+
+    <title>@yield('title')</title>
+
+</head>
+<body>
+
+@yield('content')
+
+<footer>
+    <p>Webcko &copy; 2023</p>
+</footer>
+</body>
+</html>
