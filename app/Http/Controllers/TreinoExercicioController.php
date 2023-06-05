9ccf155d4628ed7ea2e60d689b1cfea78effdcc1
LuizHenz <robertgito@hotmail.com>
2023-06-05 19:32:14 -0300

    Controller-Models-Migrations

---
+++ app/Http/Controllers/TreinoExercicioController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TreinoExercicio;
+use Illuminate\Http\Request;
+
+class TreinoExercicioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TreinoExercicio $treinoExercicio)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TreinoExercicio $treinoExercicio)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, TreinoExercicio $treinoExercicio)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TreinoExercicio $treinoExercicio)
+    {
+        //
+    }
+}
