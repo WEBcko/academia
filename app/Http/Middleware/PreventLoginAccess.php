fa7b6e45325e868dbbacb5053bf210e4a7f880b4
LuizHenz <robertgito@hotmail.com>
2023-06-07 19:52:33 -0300

    Upgrade-Login/Home

--- app/Http/Middleware/PreventLoginAccess.php
+++ app/Http/Middleware/PreventLoginAccess.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class AdminAccess extends Middleware
+class PreventLoginAccess
 {
     /**
      * Handle an incoming request.
@@ -15,11 +15,9 @@ class AdminAccess extends Middleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        //Faz a verificação se o usuario ele nao esta autenticado e se ele não é o ADM=0, caso contrario, retorna a home
-        if(!auth()->check() || auth()->user()->role != 0){
-            return redirect()->route('home');
+        if (auth()::check()) {
+            return redirect()->route('/home');
         }
         return $next($request);
-        dd('Você é ADM');
     }
 }
